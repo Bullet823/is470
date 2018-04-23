9ff12fb91eb118511a1d46fc71d4b908f093c918
Jorge Delgado <jdelgad@gmail.com>
2018-04-23 15:44:08 -0500

    comentario

--- patrones/Patrones/src/pe/up/facade/Client.java
+++ patrones/Patrones/src/pe/up/facade/Client.java
@@ -2,5 +2,6 @@ package pe.up.facade;
 public class Client{   public static void main(String[] args)   { 
 	TravelFacade facade = new TravelFacade(); 
 	//facade.getFlightsAndHotels(from, to);   
+	//ejemplo de comenrario
 	}
 }
